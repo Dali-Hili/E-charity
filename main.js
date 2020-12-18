7cbe03d1061961a93e58f386dd33aacbe4b83fb9
Dali-Hili <you@example.com>
2020-12-18 16:34:50 +0100

    website functionality

--- main.js
+++ main.js
@@ -12,10 +12,21 @@ var addinfo=function(event) {
       document.forms[0].reset()
         var olddata=localStorage.getItem('information-list');
         var newData = JSON.parse(olddata) 
-        newData.push(information)
- console.log(JSON.parse(localStorage.getItem('information-list')))
- localStorage.setItem('information-list', JSON.stringify(newData))
-       }    
+        if(Array.isArray(newData)){
+          newData.push(information)
+           localStorage.setItem('information-list', JSON.stringify(newData))
+        }
+        else{
+          newData = [];
+          newData.push(information);
+           localStorage.setItem('information-list', JSON.stringify(newData))
+
+        }
+
+        
+        
+
+       }     
 $("#sub-btn").click(addinfo)
 
 
@@ -27,6 +38,7 @@ $(".casesbutton").on("click",function(){
  $('#whyusimg').hide()
  $('#actions').hide()
  $('.partn').hide()
+ $('#contact').hide()
  $(".input").slideDown()
  $("#sub-btn").show()
 })
@@ -55,9 +67,52 @@ $('#whyuss').slideDown();
 });
 
 var informationList = JSON.parse(localStorage.getItem('information-list'))
-if (information===null){
-  
-}
 for (var i = 0; i < informationList.length; i++){
-    $(".ghassen").append(` <div class="notif"> <h2>${informationList[i].name} is intrested in this one </h2> <h2> ${informationList[i].name} </h2>    <h2>${informationList[i].email}</h2>  <h2>  ${informationList[i].phone} </h2> </div>`);
+    $(".ghassen").append(` <div class="notif"> <h2 class="client">${informationList[i].name} is intrested in this one </h2> <h2 class="client"> ${informationList[i].name} </h2>    <h2 class="client">${informationList[i].email}</h2>  <h2 class="client">  ${informationList[i].phone} </h2> </div>`);
 }
+
+$('.notif').hide()
+$('#notif').click(function () {
+$(".infoadmin").hide();
+$("#login").hide();
+// $().hide();
+$(".notif").slideDown();
+});
+
+$('.addcase').hide()
+$('#newcase').click(function () {
+$(".infoadmin").hide();
+$("#login").hide();
+$("#notif").hide();
+$(".addcase").slideDown();
+});
+
+var jdidi= []
+var addcase=function() {
+
+  // event.preventDefault()
+  
+  var cases={
+    photo:$("#photolink").val(),
+    description:$("#discription").val()
+  
+  }
+     jdidi.push(cases)
+      document.forms[0].reset()
+        var oldcase=localStorage.getItem('cases-list');
+        var newcase = JSON.parse(oldcase) 
+      if(Array.isArray(newcase)){
+      newcase.push(cases)
+      localStorage.setItem('cases-list', JSON.stringify(newcase))
+       }
+       else {
+        newcase=[]
+         newcase.push(cases);
+           localStorage.setItem('cases-list', JSON.stringify(newcase))
+       }
+    
+      
+
+       }
+  
+$("#post").click(addcase)
